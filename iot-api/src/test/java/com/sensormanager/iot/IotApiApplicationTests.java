d43441d05a04e6d6d445166e88b30a1e8e86c9ca
Cristian Aliaga <132721848+cristian-aliaga@users.noreply.github.com>
2025-04-11 00:17:03 -0400

    Pipeline Unit Test

--- iot-api/src/test/java/com/sensormanager/iot/IotApiApplicationTests.java
+++ iot-api/src/test/java/com/sensormanager/iot/IotApiApplicationTests.java
@@ -3,7 +3,7 @@ package com.sensormanager.iot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = IotApiApplication.class)
 class IotApiApplicationTests {
 
 	@Test
